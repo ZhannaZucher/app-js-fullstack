94d85a8e411d1528b70900deef177a42af47758c
ZhannaZucher <jzucher@gmail.com>
2024-02-18 10:17:21 +0100

    add: cors config

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,19 @@ const connectDB = require("./config/db")
 const port = 5000
 //allows access to var env from the entire project
 const dotenv = require("dotenv").config()
+const cors = require("cors")
 
 //DB connexion
 connectDB()
 
-
 const app = express()
+
+//CORS Authorization
+app.use(cors({
+	origin: "http://localhost:5173",
+	credentials: true,
+	optionsSuccessStatus: 200
+}))
 
 //middleware for request data treatment
 app.use(express.json())
