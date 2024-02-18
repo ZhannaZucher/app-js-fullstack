c7a66227fe82dbbdfcfe3faf26d723779f226958
ZhannaZucher <jzucher@gmail.com>
2024-02-18 10:33:38 +0100

    minor change

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -3,9 +3,6 @@ const PostModel = require("../models/post.model")
 module.exports.getPosts = async (req, res) => {
 	const posts = await PostModel.find()
 	res.status(200).json(posts)
-	if(err) {
-		res.status(400).json({error})
-	}
 }
 
 module.exports.createPost = async (req, res) => {
