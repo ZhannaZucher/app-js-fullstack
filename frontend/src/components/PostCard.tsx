51d00bcf6634347731dc8e5f17222181e78abf51
ZhannaZucher <jzucher@gmail.com>
2024-02-18 16:24:46 +0100

    feat: edit msg logic

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react"
 import { Post } from "../../types"
 import LikePost from "./LikePost"
+import axios from "axios"
 
 type PostCardProps = {
   userId: string
   post: Post
 }
 const PostCard = ({ userId, post }: PostCardProps) => {
+  const [isAuthor, setIsAuthor] = useState<boolean>(false)
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+  const [newMessage, setNewMessage] = useState("")
+
+  useEffect(() => {
+    if (post.author === userId) {
+      setIsAuthor(true)
+    } else {
+      setIsAuthor(false)
+    }
+  }, [userId, post.author])
+
+  const handleEdit = () => {
+    if (newMessage) {
+      axios.put(`http://localhost:5000/post/${post._id}`, {
+        message: newMessage,
+        author: userId,
+      })
+    }
+  }
+
   const dateFormater = (date: string) => {
     return new Date(date).toLocaleDateString("fr-FR", {
       year: "numeric",
@@ -22,9 +45,33 @@ const PostCard = ({ userId, post }: PostCardProps) => {
         <h3>{post.author}</h3>
         <p>posté le {dateFormater(post.createdAt)}</p>
       </div>
-      <p>{post.message}</p>
+      {isEditing ? (
+        <div className="edit-container">
+          <textarea
+            defaultValue={post.message}
+            onChange={(e) => setNewMessage(e.target.value)}
+          ></textarea>
+          <button
+            onClick={() => {
+              setIsEditing(false)
+              handleEdit()
+            }}
+          >
+            Valider
+          </button>
+        </div>
+      ) : (
+        <p>{newMessage ? newMessage : post.message}</p>
+      )}
       <div className="icons-part">
         <LikePost post={post} userId={userId} />
+        {isAuthor && (
+          <div className="update-delete-icons">
+            <span id="update-btn" onClick={() => setIsEditing(!isEditing)}>
+              &#10000;
+            </span>
+          </div>
+        )}
       </div>
     </div>
   )
