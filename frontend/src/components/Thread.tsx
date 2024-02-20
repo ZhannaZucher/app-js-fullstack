90638d0ed222afb7c90497846bb7d81ee6d091cc
ZhannaZucher <jzucher@gmail.com>
2024-02-20 18:57:56 +0100

    move logic to redux

--- frontend/src/components/Thread.tsx
+++ frontend/src/components/Thread.tsx
@@ -1,19 +1,25 @@
 import axios from "axios"
-import { useEffect, useState } from "react"
-import { Post } from "../../types"
+import { useEffect } from "react"
+// import { Post } from "../../types"
 import PostCard from "./PostCard"
+import { selectPosts, useAppDispatch, useAppSelector } from "../store/selectors"
+import { getPosts } from "../store/postSlice"
 
 const Thread = () => {
-  const [posts, setPosts] = useState<Post[]>([])
+  const dispatch = useAppDispatch()
+  const posts = useAppSelector(selectPosts)
 
   useEffect(() => {
-    axios.get("http://localhost:5000/post/").then((res) => setPosts(res.data))
-  }, [])
+    axios
+      .get("http://localhost:5000/post/")
+      .then((res) => dispatch(getPosts(res.data)))
+  }, [dispatch])
 
   return (
     <div className="thread-container">
       {posts
-        ?.sort((a, b) => b.createdAt.localeCompare(a.createdAt))
+        .slice()
+        .sort((a, b) => b.createdAt.localeCompare(a.createdAt))
         .map((post) => (
           <PostCard key={post._id} post={post} />
         ))}
