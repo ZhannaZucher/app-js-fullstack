91ebeb4e9270d54abfef60f546eda747d6d3a0d7
ZhannaZucher <jzucher@gmail.com>
2024-02-20 18:35:40 +0100

    add typed dispatch  & selector

---
+++ frontend/src/store/selectors.ts
@@ -0,0 +1,10 @@
+import type { RootState, AppDispatch } from "./store"
+import { useSelector, TypedUseSelectorHook, useDispatch } from "react-redux"
+
+//typed useSelector
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+//typed useDispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+
+// export const selectPictures = (state: RootState) => state.pictures.pictures
+export const selectUser = (state: RootState) => state.user.userId
